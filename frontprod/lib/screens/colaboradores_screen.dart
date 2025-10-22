a344eb6f122a1858b33fca59ca713ef1d57bd971
naxo <nachoshoko15@gmail.com>
2025-10-21 22:52:06 -0300

    test 2

--- frontprod/lib/screens/colaboradores_screen.dart
+++ frontprod/lib/screens/colaboradores_screen.dart
@@ -4,7 +4,6 @@ import 'package:excel/excel.dart' as excel_pkg;
 import '../services/api_service.dart';
 import '../models/colaborador.dart';
 
-//test
 class ColaboradoresScreen extends StatefulWidget {
   const ColaboradoresScreen({super.key});
 
